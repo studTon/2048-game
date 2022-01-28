81d20ebb50d42338b507fc8acda4c15bad7b5ccd
studTon <evertonsdasilva1@gmail.com>
2022-01-28 12:55:00 -0300

    Create game grid Create colors file

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import tkinter as tk
+from turtle import width
+import colors as c
 
 class Game(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         self.grid()
+        self.master.title("2048")
+
+        self.main_grid = tk.Frame(
+            self, bg=c.GRID_COLOR, bd=3, width=600, height=600
+        )
+        self.main_grid.grid(pady=(100, 0))
